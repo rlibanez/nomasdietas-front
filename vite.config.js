eb7d62ec4401195ad6313565dc0b0a189090208b
rlibanez <rlibanezz@gmail.com>
2024-09-10 20:21:08 +0200

    Server Proxy de Vite para backend

--- vite.config.js
+++ vite.config.js
@@ -14,5 +14,15 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  // Las peticiones /api serán redirigidas a http://localhost:8080/api
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080', // URL de tu servidor backend
+        changeOrigin: true,
+        // rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
   }
 })
